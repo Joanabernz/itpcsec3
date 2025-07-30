f828de7793a5374bd0d8a6e8a1b834c06fac9f88
Joanabernz <124427180+Joanabernz@users.noreply.github.com>
2025-07-30 17:18:02 +0800

    php

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = msqli_connect ("localhost", "root", " ", "itpc2");
+
+if (!$conn){
+    die("Wrong");
+
+}
+
+$email = $_post['email'];
+$password = $_post['password'];
+
+$sql = "Select * From students Where email = '$email' and password = '$password' ";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo("Welcome");
+    
+}
+
+else{
+    echo "Login Failed";
+}
+mysqli_close($conn)
+?>
